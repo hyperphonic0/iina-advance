791669b1c0c5d5dec318c45efab7c0aa1f65f947
Matt Svoboda <2213815+svobs@users.noreply.github.com>
2025-03-26 13:50:18 -0700

    Add in screenshot-raw code. Adjust wording. More work on Metal layer

--- iina/MPVController.swift
+++ iina/MPVController.swift
@@ -1402,11 +1402,10 @@ class MPVController: NSObject {
           break
         }
 
-        // TODO: implement parsing of screenshot-raw!
       }
 
     default:
-      // Logger.log("Unhandled mpv event: \(eventId)", level: .verbose)
+      player.log.trace("Unhandled mpv event: \(eventId)")
       break
     }
 
@@ -1417,6 +1416,32 @@ class MPVController: NSObject {
       let eventName = "mpv.\(String(cString: mpv_event_name(eventId)))"
       player.events.emit(.init(eventName))
     }
+  }
+
+  func getScreenshot(_ arg: String) -> NSImage? {
+    log.verbose("Taking screenshot-raw \(arg)")
+    var args = try! MPVNode.create(["screenshot-raw", arg])
+    defer {
+      MPVNode.free(args)
+    }
+    var result = mpv_node()
+    mpv_command_node(self.mpv, &args, &result)
+    guard let rawImage = try? MPVNode.parse(result) as? [String: Any] else { return nil }
+    mpv_free_node_contents(&result)
+    var pixelArray = rawImage["data"] as! [UInt8]
+    // According to mpv's client.h, the pixel array mpv returns arrange
+    // color data as "B8G8R8X8", whereas CGImages's data provider needs
+    // RGBA, so swap each pixel at index 0 and 2.
+    for i in 0 ..< pixelArray.count >> 2 {
+      pixelArray.swapAt(i << 2, i << 2 | 2)
+    }
+    let width = Int(truncatingIfNeeded: rawImage["w"] as! Int64)
+    let height = Int(truncatingIfNeeded: rawImage["h"] as! Int64)
+    let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+    let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+    let providerRef = CGDataProvider(data: NSData(bytes: pixelArray, length: pixelArray.count))!
+    let cgImage = CGImage(width: width, height: height, bitsPerComponent: 8, bitsPerPixel: 4 * 8, bytesPerRow: width * 4, space: rgbColorSpace, bitmapInfo: bitmapInfo, provider: providerRef, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
+    return NSImage(cgImage: cgImage!, size: NSSize(width: width, height: height))
   }
 
   // MARK: - Property listeners
--- iina/MetalVideoLayer.swift
+++ iina/MetalVideoLayer.swift
@@ -6,3 +6,22 @@
 //  Copyright © 2025 lhc. All rights reserved.
 //
 
+class MetalLayer: CAMetalLayer {
+
+  override init() {
+    super.init()
+
+    pixelFormat = .rgba16Float
+    backgroundColor = NSColor.black.cgColor
+  }
+
+  // necessary for when the layer containing window changes the screen
+  override init(layer: Any) {
+    let oldLayer = layer as! MetalLayer
+    super.init()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
