f4bfeb3a971629feac4fda43764cfb7481e4b37e
Yuze Jiang <i@uiryuu.moe>
2025-05-16 00:04:15 +0900

    keybinding: use input validator to validate the input filename

--- iina/ConfTableViewController.swift
+++ iina/ConfTableViewController.swift
@@ -609,8 +609,17 @@ extension ConfTableViewController:  NSMenuDelegate {
         }
       })
     } else {
+      let validator: Utility.InputValidator<String> = { [self] input in
+        if input.isEmpty {
+          return .valueIsEmpty
+        }
+        if confTableState.userConfDict[input] != nil || Constants.InputConf.defaults[input] != nil {
+          return .valueAlreadyExists
+        }
+        return .ok
+      }
       // Open modal dialog and prompt user
-      Utility.quickPromptPanel("config.new", sheetWindow: tableView.window) { newName in
+      Utility.quickPromptPanel("config.new", validator: validator, sheetWindow: tableView.window) { newName in
         guard !newName.isEmpty else {
           Utility.showAlert("config.empty_name", sheetWindow: self.tableView.window)
           return
