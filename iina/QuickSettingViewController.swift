Raphael005/github-slideshow

Fix external subtitle selection inconsistent, #5587 (#5588)
This commit will:
- Add a containsSubExt property to Utility that contains the file
  extensions of files that are subtitles or can contain subtitles
- Change QuickSettingViewController.loadExternalSubAction from allowing
  only subtitle files to be selected to including video files that may
  contain subtitles
- Change MainMenuActionHandler.menuLoadExternalSub from allowing all
  files to be selected to only allowing subtitle files and video files

iina/QuickSettingViewController.swift
@@ -852,8 +852,11 @@ class QuickSettingViewController: NSViewController, NSTableViewDataSource, NSTab
   @IBAction func loadExternalSubAction(_ sender: NSSegmentedControl) {
     if sender.selectedSegment == 0 {
       let currentDir = player.info.currentURL?.deletingLastPathComponent()
+      // In addition to subtitle files allow the user to choose video files as mpv will look for
+      // and load embedded subtitle streams in the video file.
       Utility.quickOpenPanel(title: "Load external subtitle", chooseDir: false, dir: currentDir,
-                             sheetWindow: player.currentWindow, allowedFileTypes: Utility.supportedFileExt[.sub]) { url in
+                             sheetWindow: player.currentWindow,
+                             allowedFileTypes: Utility.containsSubExt) { url in
         // set a delay
         self.player.loadExternalSubFile(url, delay: true)
         self.subTableView.reloadData()

hyperphonic0/iina-advance

iina/QuickSettingViewController.swift
@@ -1265,8 +1265,11 @@ class QuickSettingViewController: NSViewController, NSTableViewDataSource, NSTab
   @IBAction func loadExternalSubAction(_ sender: NSSegmentedControl) {
     if sender.selectedSegment == 0 {
       let currentDir = player.info.currentURL?.deletingLastPathComponent()
+      // In addition to subtitle files allow the user to choose video files as mpv will look for
+      // and load embedded subtitle streams in the video file.
       Utility.quickOpenPanel(title: "Load external subtitle", chooseDir: false, dir: currentDir,
-                             sheetWindow: player.window, allowedFileTypes: Utility.supportedFileExt[.sub]) { url in
+                             sheetWindow: player.window,
+                             allowedFileTypes: Utility.containsSubExt) { url in
         // set a delay
         self.player.loadExternalSubFile(url, delay: true)
         self.subTableView.reloadData()