wintion/video-player

Fix display link not running when player reused (#5655)
This commit will:
- Change PlayerCore.openMainWindow to call VideoView.displayActive if
  playback is not paused and the window has already been loaded
- Add an enableDisplayIdle internal setting to allow disabling the
  feature that detects when a display is idle and shuts down the display
  link to save energy
- Change VideoView.displayIdle to check the new setting and not shut
  down the display link if this feature has been disabled

This fixes a regression caused by commit 9f556bf which correctly removed
a call that was setting the mpv pause option.

iina/PlayerCore.swift
@@ -490,6 +490,14 @@ class PlayerCore: NSObject {
     let pause = Preference.bool(for: .pauseWhenOpen)
     mpv.setFlag(MPVOption.PlaybackControl.pause, pause ? true : false, level: .verbose)
 
+    // Normally the display link is started when MainWindowController.windowDidLoad calls initVideo.
+    // However if this player is being reused then the window will have already been loaded and
+    // windowDidLoad will not be called. If playback is not paused make sure the display link is
+    // active.
+    if !pause, mainWindow.loaded {
+      mainWindow.videoView.displayActive()
+    }
+
     // Send load file command
     info.justOpenedFile = true
     info.state = .loading

hyperphonic0/iina-advance

iina/PlayerCore.swift
@@ -1923,6 +1923,14 @@ class PlayerCore: NSObject {
         }
       }
       mpv.setFlag(MPVOption.PlaybackControl.pause, shouldPause)
+
+      // Normally the display link is started when MainWindowController.windowDidLoad calls initVideo.
+      // However if this player is being reused then the window will have already been loaded and
+      // windowDidLoad will not be called. If playback is not paused make sure the display link is
+      // active.
+      if !shouldPause, pwc.loaded {
+        pwc.videoView.displayActive()
+      }
     }
     log.verbose{"FileLoaded path=\(info.currentPlayback?.path.pii.quoted ?? "nil")"}
 