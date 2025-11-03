9ebec2cb3276e37d1809964d103278a3ab103d90
Matt Svoboda <2213815+svobs@users.noreply.github.com>
2025-11-02 21:06:51 -0800

    Delete temp script

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Place in project's base directory, then run from there.
-
-PDIR=`pwd` # project base dir
-for FILE in `ls "$PDIR"/iina/*.swift`
-do
-  sed -i '' 's/\(\s*\)log.error{\(.*\)}/\1log.error(\2)/' $FILE
-done
