3ede2d0f99db351ce2ac039c5ffd8d106067a6a1
Matt Svoboda <2213815+svobs@users.noreply.github.com>
2026-01-14 03:01:54 -0800

    Font Picker window: default to sans-serif if left blank; clean up on cancel too

--- iina/FontPickerWindowController.swift
+++ iina/FontPickerWindowController.swift
@@ -177,16 +177,18 @@ class FontPickerWindowController: NSWindowController, NSTableViewDelegate, NSTab
   }
 
   @IBAction func okBtnPressed(_ sender: AnyObject) {
-    if let block = finishedPicking {
+    if let finishedPicking {
       let otherString = otherField.stringValue
-      block(otherString)
+      let selectedFont = otherString.isEmpty ? Constants.String.mpvDefaultFont : otherString
+      finishedPicking(selectedFont)
       // remove the listener
-      finishedPicking = nil
+      self.finishedPicking = nil
     }
     self.close()
   }
 
   @IBAction func cancelBtnPressed(_ sender: AnyObject) {
+    self.finishedPicking = nil  // remove listener if any
     self.close()
   }
 
