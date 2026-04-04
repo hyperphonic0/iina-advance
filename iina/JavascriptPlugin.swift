baa51529918b851985af1e7614d2614cb399f848
low-batt <86170219+low-batt@users.noreply.github.com>
2026-04-04 03:17:26 +0000

    Log plugin name and version at startup (#5923)
    This commit will change the init method in the JavascriptPlugin class to
    emit a log message when a plugin is successfully loaded giving the name
    and version of the plugin as well as an indication if the plugin is
    disabled.
    
    The version of the plugin currently installed is critical information
    when investigating a problem as the user may not be using the latest
    version of the plugin.

--- iina/JavascriptPlugin.swift
+++ iina/JavascriptPlugin.swift
@@ -442,6 +442,7 @@ class JavascriptPlugin: NSObject {
     if (enabled) {
       registerSubProviders()
     }
+    Logger.log("Loaded JS plugin: \(name) \(version)\(enabled ? "" : " (disabled)")")
   }
 
   func registerSubProviders() {
