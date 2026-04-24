0f7470a180c63e787cb1d2c8b502c64bce2aa94d
Hechen Li <lhc199652@gmail.com>
2026-04-24 13:38:34 -0400

    Revert "ui: disable add keybinding button when not allowed"
    This reverts commit 507ffdae3936bc1e01bc23bcdbafa050af106415.

--- iina/Pages/SettingsPageKeyBindings.swift
+++ iina/Pages/SettingsPageKeyBindings.swift
@@ -72,8 +72,7 @@ fileprivate class ConfigEditor: SettingsAccessory.Base {
   let chooserPopupButton: NSPopUpButton
   let addConfBtn: NSButton
   let delConfBtn: NSButton
-  let addKeyMappingBtn: NSButton
-
+  
   var mappingController: NSArrayController
   
   static let defaultConfigMap: KeyValuePairs<String, String> = [
@@ -146,7 +145,6 @@ fileprivate class ConfigEditor: SettingsAccessory.Base {
     
     self.addConfBtn = NSButton()
     self.delConfBtn = NSButton()
-    self.addKeyMappingBtn = NSButton()
 
     super.init(l10n: l10n)
     
@@ -177,6 +175,7 @@ fileprivate class ConfigEditor: SettingsAccessory.Base {
     chooserView.addSubview(chooserStackView)
     chooserStackView.padding(.all(8))
     
+    let addKeyMappingBtn = NSButton()
     addKeyMappingBtn.translatesAutoresizingMaskIntoConstraints = false
     addKeyMappingBtn.imagePosition = .imageOnly
     addKeyMappingBtn.bezelStyle = .circular
@@ -445,7 +444,7 @@ fileprivate class ConfigEditor: SettingsAccessory.Base {
 
   private func changeButtonEnabledStatus() {
     let shouldEnableEdit = isCurrentConfigEditable()
-    [delConfBtn, addKeyMappingBtn].forEach { $0.isEnabled = shouldEnableEdit }
+    delConfBtn.isEnabled = shouldEnableEdit
   }
   
   /// Check whether or not a new config file with provided filename should be created.
