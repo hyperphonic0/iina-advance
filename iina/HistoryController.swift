Raphael005/github-slideshow

Add media title to history file (#6113)
This commit will:
- Add a title property to the PlaybackHistory class
- Add a title parameter to the HistoryController.add method
- Change PlayerCore.fileLoaded to pass the media title when calling the
  HistoryController to add an entry
- Add a description property to the PlaybackHistory class that provides
  a string representation suitable for a log message
- Change the HistoryController.add method to log the entry being added
  to the history file, if verbose logging is enabled
- Add a new logAllHistoryEntries property to HistoryController to
  disable logging of history entries by default
- Change the HistoryController.read method to log all history entries
  after reading the history file in debug builds of IINA, if enabled by
  logAllHistoryEntries

This commit will change IINA to start storing the media title in entries
in the history file for media that have an associated title. This will
provide the backend support required for the history window to be
updated to display media titles.

iina/HistoryController.swift
@@ -12,6 +12,12 @@ class HistoryController: NSObject {
 
   static let shared = HistoryController(plistFileURL: Utility.playbackHistoryURL)
 
+#if DEBUG
+  /// As logging of all history entries can produce a huge number of log messages this feature is only included in debug builds and
+  /// must be enabled by setting this property to `true` when you need investigate history file contents.
+  private static let logAllHistoryEntries = false
+#endif
+
   /// Cached copy of the playback history stored in the history file.
   ///
   /// This is accessed by both the main thread and a background thread and must be referenced under a lock.
@@ -45,6 +51,21 @@ class HistoryController: NSObject {
       self.history = history
       log("Read \(history.count) playback history entries")
       MemoryUsage.shared.logUsage("after reading history")
+
+      // As logging of all history entries can produce a huge number of log messages this feature is
+      // only included in debug builds.
+#if DEBUG
+      // IINA must be built with the property logAllHistoryEntries set to true when you want the
+      // history file contents to be logged when it is read.
+      guard HistoryController.logAllHistoryEntries, !history.isEmpty,
+            Logger.isEmitting(.verbose) else { return }
+      log("Playback history:", level: .verbose)
+      var index = 0
+      for entry in history {
+        log("History[\(index)] \(String(describing: entry))", level: .verbose)
+        index += 1
+      }
+#endif
     } catch {
       log("Failed to read playback history file \(plistURL.path): \(error)", level: .error)
     }
@@ -69,7 +90,8 @@ class HistoryController: NSObject {
   /// - Parameters:
   ///   - url: URL of the media being played.
   ///   - duration: Total duration of the media.
-  func add(_ url: URL, duration: Double) {
+  ///   - title: Title of the media (if available).
+  func add(_ url: URL, duration: Double, title: String?) {
     guard Preference.bool(for: .recordPlaybackHistory) else { return }
     $tasksOutstanding.withLock { $0 += 1 }
     queue.async { [self] in
@@ -78,7 +100,9 @@ class HistoryController: NSObject {
            let index = history.firstIndex(of: existingItem) {
           history.remove(at: index)
         }
-        history.insert(PlaybackHistory(url: url, duration: duration), at: 0)
+        let entry = PlaybackHistory(url: url, duration: duration, title: title)
+        history.insert(entry, at: 0)
+        log("Adding to history: \(String(describing: entry))", level: .verbose)
       }
       save()
       $tasksOutstanding.withLock { tasksOutstanding in

hyperphonic0/iina-advance

iina/HistoryController.swift
@@ -18,6 +18,12 @@ final class HistoryController {
   /// by skipping unnecessary operations.
   var historyEnabled: Bool = true
 
+#if DEBUG
+  /// As logging of all history entries can produce a huge number of log messages this feature is only included in debug builds and
+  /// must be enabled by setting this property to `true` when you need investigate history file contents.
+  private static let logAllHistoryEntries = false
+#endif
+
   private(set) var started = false
 
   let plistURL: URL
@@ -230,6 +236,22 @@ final class HistoryController {
       history = historyItemList
       log.verbose("Loaded playback history (entryCount=\(historyItemList.count))")
       MemoryUsage.shared.logUsage("after reading history")
+
+      // As logging of all history entries can produce a huge number of log messages this feature is
+      // only included in debug builds.
+#if DEBUG
+      // IINA must be built with the property logAllHistoryEntries set to true when you want the
+      // history file contents to be logged when it is read.
+      if HistoryController.logAllHistoryEntries, !history.isEmpty,
+         Logger.isEmitting(.verbose) {
+        log.verbose("Playback history:")
+        var index = 0
+        for entry in history {
+          log.verbose("History[\(index)] \(String(describing: entry))")
+          index += 1
+        }
+      }
+#endif
       return true
     } catch {
       log.error("Failed to load playback history file \(plistURL.path.pii.quoted): \(error)")
@@ -270,15 +292,21 @@ final class HistoryController {
     log.verbose("ReloadAll done: \(history.count) history entries & \(cachedRecentDocumentURLs.count) recentDocuments in \(sw.secElapsedString)")
   }
 
+      /// Add an entry to playback history.
+      /// - Note: The entry is added asynchronously by a background thread.
+      /// - Parameters:
+      ///   - id: PlaybackID of the media being played.
+      ///   - duration: Total duration of the media.
+      ///   - title: Title of the media (if available).
   @discardableResult
-  private func addPlayback(_ id: PlaybackID, duration: Double) -> PlaybackHistory? {
+  private func addPlayback(_ id: PlaybackID, duration: Double, title: String?) -> PlaybackHistory? {
     assert(DispatchQueue.isExecutingIn(workDQ))
     guard Preference.bool(for: .recordPlaybackHistory) else { return nil }
 
     if let existingItem = history.first(where: { $0.mpvMd5 == id.mpvMD5 }), let index = history.firstIndex(of: existingItem) {
       history.remove(at: index)
     }
-    let newEntry = PlaybackHistory(id: id, duration: duration)
+    let newEntry = PlaybackHistory(id: id, duration: duration, title: title)
     history.insert(newEntry, at: 0)
     historyListDidUpdate()
     saveHistoryToFile()
@@ -468,7 +496,8 @@ final class HistoryController {
 
   // MARK: - Playback Lifecycle Events
 
-  func savePlaybackMetaAfterFileDidLoad(for id: PlaybackID, durationSec: Double, positionSec: Double?) {
+  func savePlaybackMetaAfterFileDidLoad(for id: PlaybackID, durationSec: Double, positionSec: Double?,
+                                        title: String?) {
     guard historyEnabled else { return }
     if !started {
       log.verbose("While trying to save playback after file load: history not started! Starting now")
@@ -481,7 +510,7 @@ final class HistoryController {
         return
       }
       // 1. Update main history list
-      let historyEntry = addPlayback(id, duration: durationSec)
+      let historyEntry = addPlayback(id, duration: durationSec, title: title)
 
       // 2. IINA's [ancient] "resume last playback" feature
       // Add this now, or else welcome window will fall out of sync with history list